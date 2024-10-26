6181fd9c7c195cdba84676a62a282b71e7df8715
felix-lhz <1175906605@qq.com>
2024-10-26 20:09:45 +0800

    sub cmd_vel

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,22 @@
 #include <Arduino.h>
-#include <RPLIDAR.h>
 #include <WiFi.h>
 #include <ros.h>
-#include <sensor_msgs/LaserScan.h>
 #include <std_msgs/Float64MultiArray.h>
 #include "MecanumDriver.h"
 
 // 创建麦克纳姆轮驱动器对象
 MecanumDriver mecanum(9, 8, 12, 13, 11, 10, 46, 21);
 
-// 创建激光雷达对象
-RPLidar lidar;
-
-// 数据互斥锁
-SemaphoreHandle_t mutex;
-
 // WiFi网络名称和密码
 const char *ssid = "evalley-1";
 const char *password = "egu123456";
 
 // ROS服务器的IP地址和端口
-IPAddress server(192, 168, 1, 108);
+IPAddress server(192, 168, 1, 106);
 const uint16_t serverPort = 11411;
 
 // 创建ROS节点句柄
 ros::NodeHandle nh;
-
-// 定义发布的消息类型和话题
-sensor_msgs::LaserScan scan_msg;
-ros::Publisher scan_pub("scan", &scan_msg);
 
 // 定义订阅的消息类型和话题
 void cmdVelCallback(const std_msgs::Float64MultiArray &msg) {
@@ -50,8 +38,6 @@ ros::Subscriber<std_msgs::Float64MultiArray> vel_sub("cmd_vel", &cmdVelCallback)
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200); // 打开与电脑调试的串口
-  lidar.begin(Serial2); // 将激光雷达连接到串口2（19、20引脚）
-  mutex = xSemaphoreCreateMutex();
   mecanum.begin(); // 初始化麦克纳姆轮驱动器
   Serial.println("Mecanum driver initialized");
 
@@ -70,17 +56,16 @@ void setup() {
   // 初始化ROS节点
   nh.initNode();
   nh.subscribe(vel_sub);
-  nh.advertise(scan_pub);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (nh.connected()) {
-      Serial.println("Connected");
-  } else {
-      Serial.println("Not Connected");
-  }
+  if(nh.connected()){
   nh.spinOnce();
-
-  delay(50); // 控制激光雷达数据采集频率
+  delay(50);
+  } // 处理ROS消息
+  else{
+    Serial.println("ROS disconnected");
+    delay(500);
+  }
 }
